485fe3863b423d0a5c21fa8158629f254ce8e633
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2024-02-21 09:31:57 +0900

    mod crc-nnh-go-jp-migration

--- crc-nnh-go-jp-migration/R/crc-nnh-go-jp-migration.R
+++ crc-nnh-go-jp-migration/R/crc-nnh-go-jp-migration.R
@@ -17,24 +17,17 @@ GetTargetContent <- function(element, target_content) {
     return(NULL)
   }
   if (element_class == target_content){
-    tag_a <- element |> html_nodes("a") |> map( ~ {
+    tag_a <- element |> html_nodes("a") |> map_df( ~ {
       content <- .
+      img_src <- content |> html_nodes("img") %>% html_attr("src")
+      img <- ifelse(length(img_src) == 0, "", img_src)
       url <- content |> html_attr("href")
       rel <- content |> html_attr("rel")
       text <- ifelse(is.null(rel) | is.na(rel) | rel == "NA", html_text(content), rel)
-      res <-list(url=url, text=text)
+      res <- c(url=url, text=text, img=img)
       return(res)
     })
-    kImgTag <- "img"
-    tag_img <- element |> html_nodes(kImgTag) %>% html_attr("src")
-    if (length(tag_a) == 0) {
-      tag_a <- list(url="", text="")
-    }
-    if (length(tag_img) == 0) {
-      tag_img <- ""
-    }
-    res <- c(unlist(tag_a), list(img=tag_img))
-    return(res)
+    return(tag_a)
   }
   return(NULL)
 }
@@ -45,7 +38,8 @@ GetTargetContents <- function(elements, target_content) {
 GetTagForm <- function(html) {
 
 }
-GetTagDiv <- function(html) {
+GetTagDiv <- function(input_path) {
+  html <- input_path |> read_html()
   # <div>要素を取得する
   div_elements <- html_nodes(html, "div")
   target_class <- c("link_nmc",
@@ -55,19 +49,25 @@ GetTagDiv <- function(html) {
                     "site-content",
                     "site-title left",
                     "widget widget_nav_menu")
-  res <- target_class |> map( ~ {
-    target <- .
-    res <- div_elements |> GetTargetContents(target)
-    return(res)
-  })
-  names(res) <- target_class
+  res <- data.frame()
+  for (class_count in 1:length(target_class)) {
+    target <- target_class[class_count]
+    temp <- div_elements |> GetTargetContents(target)
+    if (length(temp) > 0) {
+      temp2 <- data.frame()
+      for (i in 1:length(temp)) {
+        temp2 <- bind_rows(temp2, temp[[i]])
+      }
+      temp2$target <- target
+      temp2$input_path <- input_path
+      res <- bind_rows(res, temp2)
+    }
+  }
   return(res)
-
 }
 
 ReadHtml <- function(input_path){
-  html <- input_path |> read_html()
-  tag_div <- html |> GetTagDiv()
+  tag_div <- input_path |> GetTagDiv()
   return(tag_div)
 }
 # ------ main ------
@@ -75,5 +75,5 @@ filelist_html <- kInputPath |> list.files(pattern = "*.html", full.names=T, recu
 index <- grep("/wp-json/", filelist_html)
 filelist_html <- filelist_html[-index]
 filenames <- filelist_html |> str_remove(str_c(kInputPath, "/")) %>% str_replace_all("/", "_")
-files <- filelist_html %>% map( ~ ReadHtml(.))
-names(files) <- filenames
+files <- filelist_html %>% map_df( ~ ReadHtml(.))
+#names(files) <- filenames
