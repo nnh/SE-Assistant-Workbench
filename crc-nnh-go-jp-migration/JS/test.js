b5744d697ce6359131bce1a319c9f6108f5ae358
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2024-02-27 11:09:53 +0900

    mod test.js

--- crc-nnh-go-jp-migration/JS/test.js
+++ crc-nnh-go-jp-migration/JS/test.js
@@ -4,33 +4,29 @@ const fs = require("fs");
 
 const pref = new logging.Preferences();
 pref.setLevel(logging.Type.BROWSER, logging.Level.DEBUG);
-
+const getReplaceList = () => {
+  const nnhUrl = "https://crc.nnh.go.jp/";
+  const wpContentJp = `${nnhUrl}wp-content/themes/NMC-CRC/images/`;
+  const wpContentEn = `${nnhUrl}en/wp-content/themes/CRC-ENG/images/`;
+  const pngList = [
+    ["logo_01_nmc.png", "NHOロゴ"],
+    ["logo_01_crc.png", "NMCロゴ"],
+    ["btn_pageTop.png", "ページトップ"],
+    ["logo_02_nmc.png", "NHOロゴ"],
+    ["logo_02_crc.png", "NMCロゴ"],
+  ];
+  const replaceTextList = new Map();
+  pngList.forEach((png) => {
+    replaceTextList.set(`${wpContentJp}${png[0]}`, png[1]);
+    replaceTextList.set(`${wpContentEn}${png[0]}`, png[1]);
+  });
+  return replaceTextList;
+};
 const getLinksFromHtmlString = (htmlString) => {
-  const replaceTextList = new Map([
-    [
-      "https://crc.nnh.go.jp/wp-content/themes/NMC-CRC/images/logo_01_nmc.png",
-      "NHOロゴ",
-    ],
-    [
-      "https://crc.nnh.go.jp/wp-content/themes/NMC-CRC/images/logo_01_crc.png",
-      "NMCロゴ",
-    ],
-    [
-      "https://crc.nnh.go.jp/wp-content/themes/NMC-CRC/images/btn_pageTop.png",
-      "ページトップ",
-    ],
-    [
-      "https://crc.nnh.go.jp/wp-content/themes/NMC-CRC/images/logo_02_nmc.png",
-      "NHOロゴ",
-    ],
-    [
-      "https://crc.nnh.go.jp/wp-content/themes/NMC-CRC/images/logo_02_crc.png",
-      "NMCロゴ",
-    ],
-  ]);
   const $ = cheerio.load(htmlString);
   const title = $("title").text();
   const links = [];
+  const replaceTextList = getReplaceList();
   $("form").each((_, element) => {
     const text = $(element)
       .find(".screen-reader-text")
@@ -69,7 +65,9 @@ const testOpenPage = async (driver, url) => {
   try {
     await driver.get(url);
     const htmlString = await driver.getPageSource();
-    const [links, title] = getLinksFromHtmlString(htmlString);
+    console.log(url);
+    const [links, title] = await getLinksFromHtmlString(htmlString);
+    console.log(title);
     writeLinksToFile(links, `${title}.txt`);
   } catch (e) {
     console.log(e);
@@ -81,13 +79,12 @@ const testWithBrowserCapabilities = async (capabilities) => {
   capabilities.setLoggingPrefs(pref);
   const driver = new Builder().withCapabilities(capabilities).build();
   const urls = ["https://crc.nnh.go.jp/", "https://crc.nnh.go.jp/en/"];
-  await Promise.all(
-    urls.map(async (url) => {
-      await testOpenPage(driver, url);
-    })
-  );
+  for (const url of urls) {
+    await testOpenPage(driver, url);
+  }
   driver && (await driver.quit());
 };
+
 const main = async () => {
   //await testWithBrowserCapabilities(Capabilities.chrome());
   //await testWithBrowserCapabilities(Capabilities.firefox());
