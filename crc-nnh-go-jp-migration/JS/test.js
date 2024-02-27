e0a79909b4746481e5ce7490deec51009573250b
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2024-02-27 10:44:31 +0900

    mod test.js

--- crc-nnh-go-jp-migration/JS/test.js
+++ crc-nnh-go-jp-migration/JS/test.js
@@ -6,20 +6,51 @@ const pref = new logging.Preferences();
 pref.setLevel(logging.Type.BROWSER, logging.Level.DEBUG);
 
 const getLinksFromHtmlString = (htmlString) => {
+  const replaceTextList = new Map([
+    [
+      "https://crc.nnh.go.jp/wp-content/themes/NMC-CRC/images/logo_01_nmc.png",
+      "NHOロゴ",
+    ],
+    [
+      "https://crc.nnh.go.jp/wp-content/themes/NMC-CRC/images/logo_01_crc.png",
+      "NMCロゴ",
+    ],
+    [
+      "https://crc.nnh.go.jp/wp-content/themes/NMC-CRC/images/btn_pageTop.png",
+      "ページトップ",
+    ],
+    [
+      "https://crc.nnh.go.jp/wp-content/themes/NMC-CRC/images/logo_02_nmc.png",
+      "NHOロゴ",
+    ],
+    [
+      "https://crc.nnh.go.jp/wp-content/themes/NMC-CRC/images/logo_02_crc.png",
+      "NMCロゴ",
+    ],
+  ]);
   const $ = cheerio.load(htmlString);
   const title = $("title").text();
   const links = [];
-  $("a").each((index, element) => {
+  $("form").each((_, element) => {
+    const text = $(element)
+      .find(".screen-reader-text")
+      .text()
+      .replace(/\t/g, "")
+      .replace(/\n/g, "");
+    const url = $(element).attr("action");
+    links.push({ title, url, text });
+  });
+
+  $("a").each((_, element) => {
     const url = $(element).attr("href");
     let text = $(element).text().replace(/\t/g, "").replace(/\n/g, "");
     if (!text) {
-      console.log("test");
       const imgSrc = $(element).find("img").attr("src");
-      text = imgSrc;
+      text = replaceTextList.has(imgSrc) ? replaceTextList.get(imgSrc) : imgSrc;
     }
     links.push({ title, url, text });
   });
-  return links;
+  return [links, title];
 };
 
 const writeLinksToFile = (links, filename) => {
@@ -34,47 +65,32 @@ const writeLinksToFile = (links, filename) => {
  * @param driver
  * @returns {Promise<void>}
  */
-const testOpenPage = async (driver) => {
+const testOpenPage = async (driver, url) => {
   try {
-    await driver.get("https://crc.nnh.go.jp/");
+    await driver.get(url);
     const htmlString = await driver.getPageSource();
-    const links = getLinksFromHtmlString(htmlString);
-    writeLinksToFile(links, "links.txt");
-    /*    for (const link of links) {
-      console.log(`${link.url}, ${link.text}`);
-      if (isValidUrl(link.url)) {
-        await driver.get(link.url, { url: link.url });
-        //  const resultHtmlString = await driver.getPageSource();
-        const statusCode = await driver.executeScript(
-          "return window.location.href"
-        );
-        console.log(statusCode);
-        //console.log(resultHtmlString);
-      } else {
-        //console.log("Invalid URL:", link.url);
-      }
-        }*/
+    const [links, title] = getLinksFromHtmlString(htmlString);
+    writeLinksToFile(links, `${title}.txt`);
   } catch (e) {
     console.log(e);
   } finally {
-    driver && (await driver.quit());
   }
 };
 
-const isValidUrl = (url) => {
-  try {
-    new URL(url);
-    return true;
-  } catch (e) {
-    return false;
-  }
-};
-
-const testWithBrowserCapabilities = (capabilities) => {
+const testWithBrowserCapabilities = async (capabilities) => {
   capabilities.setLoggingPrefs(pref);
   const driver = new Builder().withCapabilities(capabilities).build();
-  testOpenPage(driver);
+  const urls = ["https://crc.nnh.go.jp/", "https://crc.nnh.go.jp/en/"];
+  await Promise.all(
+    urls.map(async (url) => {
+      await testOpenPage(driver, url);
+    })
+  );
+  driver && (await driver.quit());
 };
-//testWithBrowserCapabilities(Capabilities.chrome());
-//testWithBrowserCapabilities(Capabilities.firefox());
-testWithBrowserCapabilities(Capabilities.safari());
+const main = async () => {
+  //await testWithBrowserCapabilities(Capabilities.chrome());
+  //await testWithBrowserCapabilities(Capabilities.firefox());
+  await testWithBrowserCapabilities(Capabilities.safari());
+};
+main();
