40a2aa25ef5b9a1eabdeef939b4ba02d914da9a6
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2024-03-27 09:26:51 +0900

    mod webpage tool

--- crc-nnh-go-jp-migration/JS/form.test.js
+++ crc-nnh-go-jp-migration/JS/form.test.js
@@ -1,9 +1,28 @@
-const { Builder, By } = require("selenium-webdriver");
+// form.test.js
+const { Builder, By, WebElement } = require("selenium-webdriver");
 const assert = require("assert");
+const fs = require("fs");
+const csvFilePath = "./user.csv";
 
+let user = null;
+function readUserFromCSV() {
+  const data = fs.readFileSync(csvFilePath, "utf8").trim();
+  const [username, password] = data.split(",");
+  user = { username, password };
+
+  console.log("ユーザー情報を読み込みました。");
+  console.log(user); // ユーザー情報を出力する
+}
+readUserFromCSV();
 let driver;
+const topPageUrl = `http://${user.username}:${user.password}@crcnnh.a-and.net/about_us/`;
+const targetUrlList = [topPageUrl];
+const target = new Map([
+  ["title", "トップページ"],
+  ["url", "https://crc.nnh.go.jp/"],
+]);
 
-describe("入力フォーム デモ", () => {
+describe("リンククリックテスト", () => {
   // テスト開始前にドライバーを起動
   beforeAll(() => {
     driver = new Builder().forBrowser("chrome").build();
@@ -11,49 +30,14 @@ describe("入力フォーム デモ", () => {
 
   // テスト終了後にドライバーを終了
   afterAll(() => driver.quit());
+  targetUrlList.forEach((url) => {
+    test("aaaa", async () => {
+      // テスト対象のページへアクセス
+      await driver.get(url);
 
-  test("名前欄の必須入力チェック その1", async () => {
-    // テスト対象のページへアクセス
-    await driver.get("https://hotel.testplanisphere.dev/ja/login.html");
-
-    // 何も入力せずにSubmitする
-    await driver.findElement(By.id("login-button")).click();
-
-    // エラーメッセージを取得して、エラー文言が正しいかチェックする
-    const errorMessage = await driver
-      .findElement(By.id("email-message"))
-      .getText();
-    assert.equal(errorMessage, "このフィールドを入力してください。");
+      // エラーメッセージを取得して、エラー文言が正しいかチェックする
+      const currentUrl = await driver.getCurrentUrl();
+      assert.equal(currentUrl, `${topPageUrl}`);
+    }, 30000); // タイムアウトを30秒に設定
   });
-  test("aaaa", async () => {
-    // テスト対象のページへアクセス
-    await driver.get("https://hotel.testplanisphere.dev/ja/index.html");
-
-    // 何も入力せずにSubmitする
-    await driver
-      .findElement(By.xpath("//a[@class='nav-link' and text()='宿泊予約']"))
-      .click();
-
-    // エラーメッセージを取得して、エラー文言が正しいかチェックする
-    const currentUrl = await driver.getCurrentUrl();
-    assert.equal(currentUrl, "https://hotel.testplanisphere.dev/ja/plans.html");
-  });
-  /*
-  test("名前欄の必須入力チェック その2", async () => {
-    // テスト対象のページへアクセス
-    await driver.get(
-      "https://ics-creative.github.io/180523_selenium_webdriver/"
-    );
-
-    // 名前を入力してSubmitする
-    await driver.findElement(By.id("name")).sendKeys("品川太郎");
-    await driver.findElement(By.id("submitButton")).click();
-
-    // エラーメッセージを取得して、エラー文言が空であるかチェックする
-    const errorMessage = await driver
-      .findElement(By.id("error_name"))
-      .getText();
-    assert.equal(errorMessage, "");
-  });
-*/
 });
