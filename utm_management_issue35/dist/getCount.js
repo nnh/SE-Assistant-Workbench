7502e0fcacd958b85ca8bfee29fc2e8549acda4d
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2024-06-05 15:45:09 +0900

    utm-issue35

--- utm_management_issue35/dist/getCount.js
+++ utm_management_issue35/dist/getCount.js
@@ -7,18 +7,22 @@ function getCount() {
   const computerNameCount = Array.from(computerSet)
     .map(pcname => inputValues.filter(([_, x]) => x === pcname))
     .filter(x => x.length > 1);
-  const targetValues = computerNameCount.map(pcinfo => {
-    const pcname = Array.from(new Set(pcinfo.map(([, pcname]) => pcname)))[0];
-    const ymdlist = pcinfo
-      .map(([ymd, _]) => new DateUtils().formatDate(ymd))
-      .join(', ');
-    const count = pcinfo.length;
-    return [pcname, ymdlist, count];
-  });
+  const targetValues = computerNameCount
+    .map(pcinfo => {
+      const pcname = Array.from(new Set(pcinfo.map(([, pcname]) => pcname)))[0];
+      const ymdlist = pcinfo.map(([ymd, _]) => new DateUtils().formatDate(ymd));
+      const ymdUnique = Array.from(new Set(ymdlist));
+      const ymdjoin = ymdUnique.join(', ');
+      const count = ymdUnique.length;
+      return [pcname, ymdjoin, count];
+    })
+    .filter(([_x, _y, cnt]) => cnt > 1);
+  outputSheet.clear();
   outputSheet.getRange(1, 1).setValue('PC名');
   outputSheet.getRange(1, 2).setValue('年月');
   outputSheet.getRange(1, 3).setValue('回数');
+  const sortedData = targetValues.sort((a, b) => b[2] - a[2]);
   outputSheet
-    .getRange(2, 1, targetValues.length, targetValues[0].length)
-    .setValues(targetValues);
+    .getRange(2, 1, sortedData.length, sortedData[0].length)
+    .setValues(sortedData);
 }
