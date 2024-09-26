31245aa3a16ad324e7de7847b677d82a7b444cb5
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2024-09-26 10:46:48 +0900

    mod 20240920oscr

--- 20240920oscr/src/20240920oscr.ts
+++ 20240920oscr/src/20240920oscr.ts
@@ -1,46 +1,19 @@
 function main(): void {
-  const parentFolderId: string | null =
-    PropertiesService.getScriptProperties().getProperty('parentFolderId');
-  if (parentFolderId === null) {
-    throw new Error('No parentFolderId');
-  }
-  const dcFolderId: string | null =
-    PropertiesService.getScriptProperties().getProperty('dcFolderId');
-  if (dcFolderId === null) {
-    throw new Error('No dcFolderId');
-  }
-  const otherFolderId: string | null =
-    PropertiesService.getScriptProperties().getProperty('otherFolderId');
-  if (otherFolderId === null) {
-    throw new Error('No otherFolderId');
-  }
-  const targetFolders: GoogleAppsScript.Drive.Folder[] = [
-    parentFolderId,
-    dcFolderId,
-    otherFolderId,
-  ].map(x => DriveApp.getFolderById(x));
-  if (targetFolders.some(x => x === null)) {
-    throw new Error('No target folder');
-  }
-  const [parentFolder, dcFolder, otherFolder] = targetFolders;
-  const dcAndOtherFolders: GoogleAppsScript.Drive.Folder[] = [
-    dcFolder,
-    otherFolder,
-  ]
-    .map(getFolders_)
+  const sheetIdColNumber: number = 3;
+  const inputSheet: GoogleAppsScript.Spreadsheet.Sheet =
+    SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
+  const sheetIdList: string[] = inputSheet
+    .getRange(1, sheetIdColNumber, inputSheet.getLastRow(), 1)
+    .getValues()
     .flat();
-  const inputFolders: GoogleAppsScript.Drive.Folder[] = [
-    parentFolder,
-    ...dcAndOtherFolders,
-  ];
-  const targetFilesInfo: string[][][] = inputFolders.map(x => getFiles_(x));
-  const spreadsheetId: string | null =
-    PropertiesService.getScriptProperties().getProperty('testSsId');
-  if (spreadsheetId === null) {
-    throw new Error('No spreadsheetId');
-  }
+  sheetIdList.forEach(sheetId => execModSpreadSheet_(sheetId));
+}
+function execModSpreadSheet_(spreadsheetId: string): void {
   const spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet =
     SpreadsheetApp.openById(spreadsheetId);
+  if (spreadsheet === null) {
+    throw new Error('No spreadsheet');
+  }
   modSpreadSheet_(spreadsheet);
 }
 function getFiles_(folder: GoogleAppsScript.Drive.Folder): string[][] {
@@ -80,9 +53,10 @@ function modSpreadSheet_(
     return;
   }
   // 年度のプルダウンリストを再作成
-  createDropdown_(targetSheet.getRange('D1'));
+  updateDropdown_(targetSheet.getRange('D1'));
   // 控除時間を追加
-  if (targetSheet.getRange('K43').getValue() === '控除時間') {
+  const setText: string = '不足時間';
+  if (targetSheet.getRange('K43').getValue() === setText) {
     return;
   }
   targetSheet
@@ -94,7 +68,7 @@ function modSpreadSheet_(
   targetSheet
     .getRange('N43')
     .setFormula('=IF($N$40+$N$41-$N$42<0,$N$40+$N$41-$N$42,"0時間00分")');
-  targetSheet.getRange('K43').setValue('控除時間');
+  targetSheet.getRange('K43').setValue(setText);
 }
 function updateDropdown_(range: GoogleAppsScript.Spreadsheet.Range): void {
   range.clearDataValidations();
@@ -109,4 +83,91 @@ function createDropdown_(range: GoogleAppsScript.Spreadsheet.Range): void {
     .requireValueInList(years, true) // プルダウンリストを作成
     .build();
   range.setDataValidation(rule);
+}
+function getTargetFileInfo(): void {
+  const outputSheet: GoogleAppsScript.Spreadsheet.Sheet =
+    SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
+  outputSheet.clear();
+  const parentFolderId: string | null =
+    PropertiesService.getScriptProperties().getProperty('parentFolderId');
+  if (parentFolderId === null) {
+    throw new Error('No parentFolderId');
+  }
+  const dcFolderId: string | null =
+    PropertiesService.getScriptProperties().getProperty('dcFolderId');
+  if (dcFolderId === null) {
+    throw new Error('No dcFolderId');
+  }
+  const otherFolderId: string | null =
+    PropertiesService.getScriptProperties().getProperty('otherFolderId');
+  if (otherFolderId === null) {
+    throw new Error('No otherFolderId');
+  }
+  const targetFolders: GoogleAppsScript.Drive.Folder[] = [
+    parentFolderId,
+    dcFolderId,
+    otherFolderId,
+  ].map(x => DriveApp.getFolderById(x));
+  if (targetFolders.some(x => x === null)) {
+    throw new Error('No target folder');
+  }
+  const [parentFolder, dcFolder, otherFolder] = targetFolders;
+  const dcAndOtherFolders: GoogleAppsScript.Drive.Folder[] = [
+    dcFolder,
+    otherFolder,
+  ]
+    .map(getFolders_)
+    .flat();
+  const parentFolderList: string[][] = getFiles_(parentFolder);
+  const dcAndOtherFolderList: string[][] = dcAndOtherFolders
+    .map(x => getFiles_(x))
+    .flat();
+  const targetFilesInfo: string[][] = [
+    ...parentFolderList,
+    ...dcAndOtherFolderList,
+  ];
+  const targets: string[][] = targetFilesInfo
+    .map(x => {
+      const [folderId, folderName, fileId, fileName] = x;
+      if (/勤務時間計算表原本.*$/.test(fileName)) {
+        return x;
+      }
+      const filename = `^[0-9]{4}.*${folderName}.*$`;
+      if (new RegExp(filename).test(fileName)) {
+        return x;
+      }
+      return null;
+    })
+    .filter(x => x !== null);
+  const targetFolderIdList: string[] = Array.from(
+    new Set(targets.map(x => x[0]))
+  );
+  const outputValues: string[][] = targetFolderIdList
+    .map(folderId => {
+      const temp: string[][] = targets.filter(([x]) => x === folderId);
+      const years: number[] = temp
+        .map(([, , , fileName]) => {
+          if (fileName === null) {
+            return null;
+          }
+          const year = fileName.match(/^[0-9]{4}/);
+          if (year === null) {
+            return null;
+          }
+          return Number(year[0]);
+        })
+        .filter((x): x is number => x !== null);
+      if (years.length === 0) {
+        return temp;
+      }
+      const maxYear: number = Math.max(...years);
+      const res: string[][] = temp.filter(([, folderName, , fileName]) =>
+        new RegExp(`^${maxYear}.*${folderName}.*$`).test(fileName)
+      );
+      return res;
+    })
+    .flat();
+  outputSheet
+    .getRange(1, 1, outputValues.length, outputValues[0].length)
+    .setValues(outputValues);
 }
