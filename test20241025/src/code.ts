fea0e529cea93cd8383e0e3553e29ea3cf321617
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2024-10-28 15:44:55 +0900

    mod

--- test20241025/src/code.ts
+++ test20241025/src/code.ts
@@ -15,6 +15,7 @@
  */
 const constNoRef: string = '参照なし';
 const constRef1Header: string[][] = [['参照元シート', '参照先シート']];
+const constSheetListSheetName: string = 'シート名';
 function getOutputSheet_(
   sheetName: string
 ): GoogleAppsScript.Spreadsheet.Sheet {
@@ -43,7 +44,7 @@ function getRefSheetList_(
   }
   const res: string[][] = setTest.map(x => [
     sheet.getName(),
-    x.replace('!', ''),
+    x.replace("'!", '').replace('!', ''),
   ]);
   return res;
 }
@@ -60,8 +61,9 @@ function test20241025() {
   const sheets: string[][] = getSheetList_(ss);
   // 参照の一覧を取得
   const refList: string[][] = getRefList1_(ss, sheets);
+  getRefList2(refList);
 }
-function getRefList2(inputData: string[][] | null = null) {
+function getRefList2(inputData: string[][] | null = null): void {
   const ref1Sheet: GoogleAppsScript.Spreadsheet.Sheet | null =
     SpreadsheetApp.getActiveSpreadsheet().getSheetByName('参照情報１');
   if (ref1Sheet === null) {
@@ -82,15 +84,19 @@ function getRefList2(inputData: string[][] | null = null) {
   const refToSheetUnique: string[][] = Array.from(refToSheetUniqueSet).map(
     x => [x]
   );
-  const outputSheetRef: GoogleAppsScript.Spreadsheet.Sheet =
+  const outputSheetRef2: GoogleAppsScript.Spreadsheet.Sheet =
     getOutputSheet_('参照情報２');
-  outputSheetRef.clear();
-  outputSheetRef
-    .getRange(1, 1, 1, 2)
-    .setValues([['参照されているシート', '参照元も参照先もないシート']]);
-  outputSheetRef
+  outputSheetRef2.clear();
+  outputSheetRef2.getRange(1, 1, 1, 1).setValues([['参照されているシート']]);
+  outputSheetRef2
     .getRange(2, 1, refToSheetUnique.length, refToSheetUnique[0].length)
     .setValues(refToSheetUnique);
+  const outputSheetRef3: GoogleAppsScript.Spreadsheet.Sheet =
+    getOutputSheet_('参照情報３');
+  outputSheetRef3.clear();
+  outputSheetRef3
+    .getRange(1, 1, 1, 2)
+    .setValues([['参照元も参照先もないシート', 'カテゴリ']]);
   const nonRefSheets: string[][] = ref1Value
     .map(([refFromSheet, refToSheet]) => {
       if (refToSheetUniqueSet.has(refFromSheet)) {
@@ -99,19 +105,54 @@ function getRefList2(inputData: string[][] | null = null) {
       if (refToSheet !== constNoRef) {
         return null;
       }
-      return [refFromSheet, refToSheet];
+      const category = setCategory_(refFromSheet);
+      return [refFromSheet, category];
     })
     .filter(x => x !== null);
 
-  outputSheetRef
-    .getRange(2, 2, nonRefSheets.length, nonRefSheets[0].length)
+  outputSheetRef3
+    .getRange(2, 1, nonRefSheets.length, nonRefSheets[0].length)
     .setValues(nonRefSheets);
+}
+function setCategory_(sheetname: string): string {
+  if (
+    sheetname === 'Base' ||
+    sheetname === 'Datacenter' ||
+    sheetname === 'Stat'
+  ) {
+    return 'マスタ';
+  }
+  if (
+    sheetname === 'Usage' ||
+    sheetname === '委託予定' ||
+    sheetname === 'Application'
+  ) {
+    return sheetname;
+  }
+  if (new RegExp('^DCtrialslist').test(sheetname) || sheetname === 'Members') {
+    return 'DCtrialslist';
+  }
+  if (sheetname === 'ARO支援一覧test') {
+    return 'ARO支援一覧test';
+  }
+  if (
+    sheetname === 'Journal Information' ||
+    sheetname === 'fromHtml' ||
+    /^Form[2-4]印刷$/.test(sheetname) ||
+    sheetname === 'pubmedData' ||
+    sheetname === 'explanation'
+  ) {
+    return 'Publication';
+  }
+  return 'Others';
 }
 function getRefList1_(
   ss: GoogleAppsScript.Spreadsheet.Spreadsheet,
   sheets: string[][]
 ): string[][] {
-  const targetSheets: string[] = sheets.map(sheet => `${sheet[0]}!`);
+  const targetSheets1: string[] = sheets.map(sheet => `${sheet[0]}!`);
+  const targetSheets2: string[] = sheets.map(sheet => `'${sheet[0]}'!`);
+  const targetSheets: string[] = [...targetSheets1, ...targetSheets2];
   const refList: string[][] = sheets
     .map(sheet => {
       const refSheet: GoogleAppsScript.Spreadsheet.Sheet | null =
@@ -135,11 +176,46 @@ function getSheetList_(
   ss: GoogleAppsScript.Spreadsheet.Spreadsheet
 ): string[][] {
   const sheets: string[][] = ss.getSheets().map(sheet => [sheet.getName()]);
-  const outputSheetList: GoogleAppsScript.Spreadsheet.Sheet =
-    getOutputSheet_('シート名');
+  const outputSheetList: GoogleAppsScript.Spreadsheet.Sheet = getOutputSheet_(
+    constSheetListSheetName
+  );
   outputSheetList.clear();
   outputSheetList
     .getRange(1, 1, sheets.length, sheets[0].length)
     .setValues(sheets);
   return sheets;
+}
+function getMoveSheetList(sheetList = null) {
+  let inputSheetList: string[][];
+  if (sheetList === null) {
+    const sheetListSheet: GoogleAppsScript.Spreadsheet.Sheet | null =
+      SpreadsheetApp.getActiveSpreadsheet().getSheetByName(
+        constSheetListSheetName
+      );
+    if (sheetListSheet === null) {
+      throw new Error(`No sheet named ${constSheetListSheetName}`);
+    }
+    inputSheetList = sheetListSheet
+      .getRange(1, 1, sheetListSheet.getLastRow(), 1)
+      .getValues();
+  } else {
+    inputSheetList = sheetList;
+  }
+  const outputValues: string[][] = inputSheetList.map(sheetname => [
+    sheetname[0],
+    setCategory_(sheetname[0]),
+  ]);
+
+  const outputSheet: GoogleAppsScript.Spreadsheet.Sheet | null =
+    SpreadsheetApp.getActiveSpreadsheet().getSheetByName(
+      '移動可能シート洗い出し'
+    );
+  if (outputSheet === null) {
+    throw new Error('No sheet named 移動可能シート洗い出し');
+  }
+  outputSheet.clear();
+  outputSheet.getRange(1, 1, 1, 2).setValues([['シート名', 'カテゴリ']]);
+  outputSheet
+    .getRange(2, 1, outputValues.length, outputValues[0].length)
+    .setValues(outputValues);
 }
