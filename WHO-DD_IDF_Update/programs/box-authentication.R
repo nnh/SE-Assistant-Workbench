de75995a271dcb593b368b5eebfdd4273d27d0f6
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2025-09-10 10:10:57 +0900

    fix: resolve Box authentication error

--- WHO-DD_IDF_Update/programs/box-authentication.R
+++ WHO-DD_IDF_Update/programs/box-authentication.R
@@ -2,7 +2,7 @@
 #' 
 #' @file box-authentication.R
 #' @author Mariko Ohtsuka
-#' @date 2024.8.8
+#' @date 2025.9.9
 rm(list=ls())
 # ------ libraries ------
 # https://github.com/cloudyr/aws.s3/issues/354
@@ -10,10 +10,15 @@ rm(list=ls())
 if (!requireNamespace("aws.s3", quietly=T)) {
   install.packages("aws.s3", repos = c("https://RForge.net", "https://cloud.R-project.org"))
 }
+if (!requireNamespace("httpuv", quietly = TRUE)) {
+  install.packages("httpuv")
+}
+
 library(here)
 # ------ constants ------
 # ------ functions ------
 source(here("programs", "functions", "common.R"),  encoding="UTF-8")
 # ------ main ------
 ### Box authenticate ###
+options(httr_oob_default = FALSE)
 BoxAuth()
