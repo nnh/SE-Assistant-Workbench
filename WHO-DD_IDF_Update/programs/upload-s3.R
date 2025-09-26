73a0699678b9941df5f1b73b40a94a2a29341723
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2025-09-26 14:15:57 +0900

    issue #115, source cleaning

--- WHO-DD_IDF_Update/programs/upload-s3.R
+++ WHO-DD_IDF_Update/programs/upload-s3.R
@@ -1,18 +1,18 @@
 #' upload s3
-#' 
+#'
 #' @file upload-s3.R
 #' @author Mariko Ohtsuka
-#' @date 2024.7.24
+#' @date 2025.9.26
 # ------ libraries ------
-rm(list=ls())
+rm(list = ls())
 library(here)
 # ------ constants ------
 # ------ functions ------
-source(here("programs", "functions", "common.R"),  encoding="UTF-8")
-source(here("programs", "functions", "s3-functions.R"),  encoding="UTF-8")
-source(here("programs", "functions", "unzip-functions.R"),  encoding="UTF-8")
-source(here("programs", "functions", "whodd-idf-functions.R"),  encoding="UTF-8")
-source(here("programs", "functions", "download-box.R"),  encoding="UTF-8")
+source(here("programs", "functions", "common.R"), encoding = "UTF-8")
+source(here("programs", "functions", "s3-functions.R"), encoding = "UTF-8")
+source(here("programs", "functions", "unzip-functions.R"), encoding = "UTF-8")
+source(here("programs", "functions", "whodd-idf-functions.R"), encoding = "UTF-8")
+source(here("programs", "functions", "download-box.R"), encoding = "UTF-8")
 # ------ main ------
 # download the ZIP file from BOX.
 whodd_zip <- whoddDownloadFilesFromBox()
@@ -25,8 +25,10 @@ whoddDir <- "/WHODD/" %>% str_c(awsDirName, .)
 idfVersion <- temp$version
 targetIdfInfo <- GetIdfDownloadFilesInfoFromBox()
 for (i in 1:nrow(targetIdfInfo)) {
-  idf_zip <- targetIdfInfo[i, "id", drop=T] |> flatten_chr() |> box_dl(downloads_path, overwrite=T)  
-  idf_password <- targetIdfInfo[i, "password", drop=T]
+  idf_zip <- targetIdfInfo[i, "id", drop = T] |>
+    flatten_chr() |>
+    box_dl(downloads_path, overwrite = T)
+  idf_password <- targetIdfInfo[i, "password", drop = T]
   temp <- UnzipIdf(idf_zip, idf_password)
   checkTargetYMD <- temp |> findFolder(str_c(idfVersion, "提供"))
   if (length(checkTargetYMD) > 0) {
@@ -39,12 +41,13 @@ if (!exists("idfUnzipDir") | !exists("whoddUnzipDir")) {
   stop("unzip error.")
 }
 # upload to s3.
-copyTargetList = list(
-  list(fromName="全件.txt", toName="data.txt", toDir=idfDir, fromDir=idfUnzipDir),
-  list(fromName="英名＜可変長＞.txt", toName="full_en.txt", toDir=idfDir, fromDir=idfUnzipDir),
-  list(fromName="全件＜可変長＞.txt", toName="full_ja.txt", toDir=idfDir, fromDir=idfUnzipDir),
-  list(fromName="IDMapping.csv", toName="IDMapping.csv", toDir=whoddDir, fromDir=whoddUnzipDir),
-  list(fromName="WHODDsGenericNames.csv", toName="WHODDsGenericNames.csv", toDir=whoddDir, fromDir=whoddUnzipDir)
+copyTargetList <- list(
+  list(fromName = "全件.txt", toName = "data.txt", toDir = idfDir, fromDir = idfUnzipDir),
+  list(fromName = "英名＜可変長＞.txt", toName = "full_en.txt", toDir = idfDir, fromDir = idfUnzipDir),
+  list(fromName = "全件＜可変長＞.txt", toName = "full_ja.txt", toDir = idfDir, fromDir = idfUnzipDir),
+  list(fromName = "IDMapping.csv", toName = "IDMapping.csv", toDir = whoddDir, fromDir = whoddUnzipDir),
+  list(fromName = "WHODDsGenericNames.csv", toName = "WHODDsGenericNames.csv", toDir = whoddDir, fromDir = whoddUnzipDir),
+  list(fromName = "Version.txt", toName = "Version.txt", toDir = whoddDir, fromDir = whoddUnzipDir)
 )
 copyFiles <- GetCopyFileInfo(copyTargetList)
 UploadToS3(copyFiles)
