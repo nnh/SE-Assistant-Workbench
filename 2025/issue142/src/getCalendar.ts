a03bc974ba364c97a1a68ee87b89e34803965efb
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2026-01-20 14:02:35 +0900

    issue #142

---
+++ 2025/issue142/src/getCalendar.ts
@@ -0,0 +1,109 @@
+/**
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Lists the calendars shown in the user's calendar list.
+ * @see https://developers.google.com/calendar/api/v3/reference/calendarList/list
+ */
+export function listCalendars_() {
+  let calendars;
+  let pageToken;
+  let result: Array<[string, string]> = [];
+  do {
+    const calendarClass: GoogleAppsScript.Calendar | undefined = Calendar;
+    if (!calendarClass) {
+      throw new Error('Calendar service is not available.');
+    }
+    calendars = calendarClass.CalendarList.list({
+      maxResults: 100,
+      pageToken: pageToken,
+    });
+    if (!calendars.items || calendars.items.length === 0) {
+      console.log('No calendars found.');
+      return;
+    }
+    // Print the calendar id and calendar summary
+    for (const calendar of calendars.items) {
+      const summary = calendar.summary || 'No Title';
+      const id = calendar.id || 'No ID';
+      result.push([summary, id]);
+    }
+    pageToken = calendars.nextPageToken;
+  } while (pageToken);
+  return result;
+}
+
+export function getCalendars_(id: string): string[][] {
+  // Avoid rate limits by sleeping for 2000ms before fetching events
+  Utilities.sleep(2000);
+  const calendar = CalendarApp.getCalendarById(id);
+  if (!calendar) {
+    throw new Error(`Calendar with ID ${id} not found.`);
+  }
+  const today = new Date();
+  const events = calendar.getEvents(today, new Date('2026-03-31'));
+  if (events.length === 0) {
+    console.log('No upcoming events found.');
+    return [[id, 'No Events', '', '', '', '', '', '']];
+  }
+  const eventsInfo: string[][] = events
+    .map(event => {
+      if (event.getGuestList(false).length === 0) {
+        return null;
+      }
+      const temp: string[] = event
+        .getGuestList(false)
+        .map(guest => guest.getEmail());
+      const checkTarget = temp.filter(guest => guest !== id);
+      if (checkTarget.length === 0) {
+        return null;
+      }
+      const guestList = temp.map(guest => guest).join(', ');
+      const startTime = event.getStartTime();
+      const endTime = event.getEndTime();
+      const startDay = Utilities.formatDate(
+        startTime,
+        Session.getScriptTimeZone(),
+        'yyyy/MM/dd'
+      );
+      const startTimeStr = Utilities.formatDate(
+        startTime,
+        Session.getScriptTimeZone(),
+        'HH:mm'
+      );
+      const endTimeStr = Utilities.formatDate(
+        endTime,
+        Session.getScriptTimeZone(),
+        'HH:mm'
+      );
+      const endDay = Utilities.formatDate(
+        endTime,
+        Session.getScriptTimeZone(),
+        'yyyy/MM/dd'
+      );
+      return [
+        id,
+        event.getId(),
+        event.getTitle(),
+        startDay,
+        startTimeStr,
+        endDay,
+        endTimeStr,
+        guestList,
+      ];
+    })
+    .filter(info => info !== null) as string[][];
+  return eventsInfo;
+}
---
+++ 2025/issue142/src/index.ts
@@ -0,0 +1,105 @@
+/**
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { hello } from './example-module';
+import { listCalendars_, getCalendars_ } from './getCalendar';
+import { getOrCreateSheet_, getSheetOrNull_ } from './spreadsheetUtils';
+const TARGET_CALENDAR_IDS_SHEET_NAME = '対象カレンダーリスト';
+
+function listEvents() {
+  const targetCalendarIdsSheet: GoogleAppsScript.Spreadsheet.Sheet | null =
+    getSheetOrNull_(TARGET_CALENDAR_IDS_SHEET_NAME);
+  if (!targetCalendarIdsSheet) {
+    throw new Error(`Sheet "${TARGET_CALENDAR_IDS_SHEET_NAME}" not found.`);
+  }
+  const targetCalendars: string[][] = targetCalendarIdsSheet
+    .getDataRange()
+    .getValues();
+  const excluedTarget = targetCalendars.filter(row => row[2] === '除外');
+  const excluedTargetIds: RegExp[] = [];
+  if (excluedTarget.length > 0) {
+    for (let i = 0; i < excluedTarget.length; i++) {
+      excluedTargetIds.push(new RegExp(excluedTarget[i][1] as string));
+    }
+  }
+  const excluedConstIds: RegExp[] = [
+    new RegExp('ja.japanese#holiday@group.v.calendar.google.com'),
+    new RegExp('test.for.shareddrive@nnh.go.jp'),
+  ];
+  const excluedIds: RegExp[] = [...excluedTargetIds, ...excluedConstIds];
+  const targetCalendarIdList: string[] = targetCalendars
+    .slice(1) // 1行目（ヘッダー）を除外
+    .map(row => row[1] as string)
+    .filter(id => !excluedIds.some(regex => regex.test(id)));
+  if (targetCalendarIdList.length === 0) {
+    console.log('No target calendar IDs found.');
+    return;
+  }
+  const outputSheet: GoogleAppsScript.Spreadsheet.Sheet | null =
+    getSheetOrNull_('イベント一覧');
+  if (!outputSheet) {
+    throw new Error('Sheet "イベント一覧" not found.');
+  }
+  let outputRowNumber = outputSheet.getLastRow() + 1;
+  // 対象のカレンダーIDを配列で指定します
+  const targetCalendarIds = targetCalendarIdList;
+  const header = [
+    'id',
+    'eventId',
+    'title',
+    'startDay',
+    'startTime',
+    'endDay',
+    'endTime',
+    'guests',
+  ];
+  outputSheet.getRange(1, 1, 1, header.length).setValues([header]);
+  if (outputRowNumber === 1) {
+    outputRowNumber++;
+  }
+  for (let i = 0; i < targetCalendarIds.length; i++) {
+    const eventsArray: string[][] = getCalendars_(targetCalendarIds[i]);
+    if (eventsArray.length === 0) {
+      continue;
+    }
+    outputSheet
+      .getRange(outputRowNumber, 1, eventsArray.length, header.length)
+      .setValues(eventsArray);
+    outputRowNumber += eventsArray.length;
+    console.log(`Finished processing calendar ID: ${targetCalendarIds[i]}`);
+  }
+}
+
+function listCalendars() {
+  // 実行ユーザのマイカレンダー配下のカレンダー一覧を取得してスプレッドシートに出力します
+  const outputSheet: GoogleAppsScript.Spreadsheet.Sheet = getOrCreateSheet_(
+    TARGET_CALENDAR_IDS_SHEET_NAME
+  );
+  const calendars = listCalendars_();
+  if (!calendars) {
+    outputSheet.getRange(1, 1).setValue('カレンダーが見つかりませんでした。');
+    return;
+  }
+  // ヘッダー行の設定
+  outputSheet.getRange(1, 1).setValue('名前');
+  outputSheet.getRange(1, 2).setValue('ID');
+  outputSheet.getRange(1, 3).setValue('処理対象外にする場合は"除外"と記載');
+  // カレンダー情報の出力
+  outputSheet.getRange(2, 1, calendars.length, 2).setValues(calendars);
+}
+
+// Example usage
+console.log(hello());
